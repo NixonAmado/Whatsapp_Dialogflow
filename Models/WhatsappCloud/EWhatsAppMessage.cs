47c272b8a2a75524e64cce359c31437e354c995b
Nixon Amado <bsuarez@qnt.com.co>
2024-11-01 23:45:41 -0500

    Clase de mapeo whatsapp a dialogflow

---
+++ Models/WhatsappCloud/EWhatsAppMessage.cs
@@ -0,0 +1,9 @@
+﻿namespace WebHook8.Models.WhatsappCloud
+{
+    public class EWhatsAppMessage
+    {
+        public string? UserNumber { get; set; }
+        public string? UserText { get; set; }
+
+    }
+}
